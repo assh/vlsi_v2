c6e01090ffda59b989cd6c3fbe5177f85cc43b7e
assh <asish.panda13@gmail.com>
2020-10-13 15:35:51 +0400

    Data commit

--- parse.py
+++ parse.py
@@ -1,1 +1,10 @@
 import csv
+import pandas as pd
+
+file = pd.read_csv(r'74181.isc')
+file.to_csv (r'74181.csv', index=None)
+
+with open('74181.csv') as csv_file:
+    reader = csv.reader(csv_file,delimiter=' ')
+    for row in reader:
+        print(row)
