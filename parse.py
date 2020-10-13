98ccf793728ceb0c0487afad10136bc3cd5d83bc
assh <asish.panda13@gmail.com>
2020-10-13 16:13:43 +0400

    Added inputs.txt

--- parse.py
+++ parse.py
@@ -2,9 +2,19 @@ import csv
 import pandas as pd
 
 file = pd.read_csv(r'74181.isc')
-file.to_csv (r'74181.csv', index=None)
+file.to_csv (r'kl_input.csv', index=None)
 
-with open('74181.csv') as csv_file:
+with open('kl_input.csv') as csv_file:
+    files = open('inputs.txt','a')
     reader = csv.reader(csv_file,delimiter=' ')
     for row in reader:
         print(row)
+        if (row[2] == 'inpt'):
+            strr = ""
+            for i in range(len(row)):
+                strr = strr+row[i]+' '
+            strr = strr+'\n'
+            files.write(strr)
+
+    files.close()
+
